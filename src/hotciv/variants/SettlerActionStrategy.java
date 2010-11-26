bdda70e39d21471803d6b737be04f82f154ab667
Jacob Harris Cryer Kragh <jhckragh@camel01.daimi.au.dk>
2010-11-26 11:57:20 +0100

    Split SettlerAndArcherActionStrategy up, implemented SemiCiv.

--- src/hotciv/variants/SettlerActionStrategy.java
+++ src/hotciv/variants/SettlerActionStrategy.java
@@ -3,17 +3,13 @@ package hotciv.variants;
 import hotciv.framework.*;
 import hotciv.common.*;
 
-public class SettlerAndArcherActionStrategy implements ActionStrategy {
+public class SettlerActionStrategy implements ActionStrategy {
     public Unit performUnitActionAt(Game game, Position pos) {
         Unit u = game.getUnitAt(pos);
 	if (u.getTypeString().equals(GameConstants.SETTLER)) {
 	    game.addCityAt(pos, new CityImpl(u.getOwner()));
 	    return null;
 	}
-        if (u.isFortified()) {
-            return u.withFortify(false, u.getDefensiveStrength()/2);
-        } else {
-            return u.withFortify(true, u.getDefensiveStrength()*2);
-        }
+        return u;
     }
 }
